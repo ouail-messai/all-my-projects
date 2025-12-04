a5ab2097e9efe73f8b33cee4a1032e5bfb66f54f
ouail-messai <ouail.messai@gmail.com>
2025-12-04 06:58:54 +0100

    Source File

---
+++ Project 1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Game.h";
+using namespace std;
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	Game::StratGame();
+
+	system("pause>0");
+	system("color ljkj");
+
+	return 0;
+}
